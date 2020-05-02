5de5d1c36a5e06195beba4629d17353b9fe8ee81
leonardo <leonardo.giannini@cern.ch>
2020-05-03 01:11:36 +0200

    lt gt

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,12 +2,8 @@
 echo ":_:_:_:_:_:_:_:_:_:_:_"
 echo "Hello and welcome to this guessing game"
 echo ":_:_:_:_:_:_:_:_:_:_:_"
-
 echo ""
-
-
 sleep 2
-
 echo -ne "The big question is\r"
 sleep 0.6
 echo -ne "The big question is: how many files\r"
@@ -15,10 +11,8 @@ sleep 0.6
 echo -ne "The big question is: how many files are there\r"
 sleep 0.6
 echo -ne "The big question is: how many files are there in the current directory?"
-
 echo ""
 sleep 2
-
 
 function fff {
   echo "HAHAHA";
@@ -41,6 +35,12 @@ do
 		sleep 2
 		echo "BIG YES!!!"
 		aaa=0
+        elif  [[ $response -lt `eval fcounter` ]]
+	then
+		echo "your answer is too low: less than the # of files"
+	elif [[ $response -gt `eval fcounter` ]]
+	then
+		echo "your answer is too high: greater than the # of files"
 	fi
 done
 fff
